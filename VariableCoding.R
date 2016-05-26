12d66ca772032f2c6933b90963184fe115b87726
Jackson <jackson.kontny@analytehealth.com>
2016-05-26 08:55:15 -0500

    Update notes document with coded variable info and start coded variable R script

---
+++ VariableCoding.R
@@ -0,0 +1,8 @@
+# You need to have the 'thads' dataset
+load("~/Documents/DePaul/CSC 423/Final Project/thads.Rda")
+
+# coded variables we will use include:
+# AGE1, ZADEQ, REGION, METRO3, BDRMS, BUILT, TYPE/STRUCTURETYPE, ROOMS, TENURE/OWNRENT
+# NUNITS, PER
+
+# Perform the coding:
