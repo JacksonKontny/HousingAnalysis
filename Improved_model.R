bfa0be87d21d374f1553c5cc11812969767bf403
tablet4tn <tablet4tn@gmail.com>
2016-06-05 13:11:31 -0500

    Update Improved_model.R

--- Improved_model.R
+++ Improved_model.R
@@ -10,9 +10,6 @@ complete.model= lm(
     PER +
     PER_SQ +
     ROOMS +
-    ROOMS_BEDS +    
-    ROOMS_PERSON +
-    ROOMS_SQ +    
     struc1 + 
     struc3 + 
     struc4 + 
@@ -26,28 +23,29 @@ complete.model= lm(
 )
 summary(complete.model)
 
-prediction = data.frame(ABL80= (.8 * .75 * 70746),
-                        AGE1=26,
-                        APLMED= (.75 * 70746),
-                        B40s50s=0, 
-                        B2000s=0,
-                        B2010s=0,
-                        BEDRMS=1,
-                        ROOMS_BEDS=2,
-                        ROOMS_PERSON=2,
-                        PER=1,
-                        PER_SQ=1,
-                        ROOMS=2,
-                        ROOMS_SQ=4,
-                        struc1=0,
-                        struc3=1,
-                        struc4=0,
-                        struc5=0,
-                        TOTSAL=70746,
-                        TOTSAL_PERSON=70746,
-                        UTILITY=40,
-                        UTILITY_SQ=1600,
-                        ZINC2=70746,
-                        ZSMHC=1100
+vif(complete.model)
+#Vifs are acceptable.
+
+prediction = data.frame(
+  ABL80= (.8 * .75 * 70746),
+  AGE1=26,
+  APLMED= (.75 * 70746),
+  B40s50s=0, 
+  B2000s=0,
+  B2010s=0,
+  BEDRMS=1,
+  PER=1,
+  PER_SQ=1,
+  ROOMS = 5,
+  struc1=0,
+  struc3=1,
+  struc4=0,
+  struc5=0,
+  TOTSAL=70746,
+  TOTSAL_PERSON=70746,
+  UTILITY=40,
+  UTILITY_SQ=1600,
+  ZINC2=70746,
+  ZSMHC=1100
 )
 predict(complete.model, prediction, interval="confidence", level=.95)
