a119f6642739d3b9c32316cb76356cb5e5c94bb0
Daniel Glownia <dglownia222@gmail.com>
2016-06-06 00:55:16 -0500

    Add the hetero

--- Improved_model.R
+++ Improved_model.R
@@ -1,4 +1,4 @@
-complete.model= lm(
+m = lm(
   VALUE ~ 
     ABL80 + 
     AGE1 + 
@@ -6,20 +6,108 @@ complete.model= lm(
     B2000s +
     B2010s + 
     BEDRMS + 
-    PER_SQ +
+    PER +
     ROOMS +
     struc1 + 
     struc3 + 
     struc4 + 
     struc5 + 
-    TOTSAL_PERSON +     
+    TOTSAL +     
     UTILITY + 
-    UTILITY_SQ +    
     ZINC2 + 
     ZSMHC, 
   data = thads
 )
-summary(complete.model)
+summary(m)
+
+
+# Heteroscedasticity
+alpha = .05
+median = median(thads$ABL80); median
+sample1 = thads[thads$ABL80<median,]  # rows where EXP less than median
+sample2 = thads[thads$ABL80>=median,]
+
+
+model1= lm(
+  VALUE ~ 
+    ABL80 + 
+    AGE1 + 
+    B40s50s + 
+    B2000s +
+    B2010s + 
+    BEDRMS + 
+    PER +
+    ROOMS +
+    struc1 + 
+    struc3 + 
+    struc4 + 
+    struc5 + 
+    TOTSAL +     
+    UTILITY + 
+    UTILITY +    
+    ZINC2 + 
+    ZSMHC, 
+  data = sample1
+)
+model2= lm(
+  VALUE ~ 
+    ABL80 + 
+    AGE1 + 
+    B40s50s + 
+    B2000s +
+    B2010s + 
+    BEDRMS + 
+    PER +
+    ROOMS +
+    struc1 + 
+    struc3 + 
+    struc4 + 
+    struc5 + 
+    TOTSAL +     
+    UTILITY + 
+    UTILITY +    
+    ZINC2 + 
+    ZSMHC, 
+  data = sample2
+)
+
+df1 = summary(model1)$df[2]
+mse1 = summary(model1)$sigma ^ 2
+df1; 
+
+df2 = summary(model2)$df[2]
+mse2 = summary(model2)$sigma ^ 2
+df2; 
+
+if (mse1 > mse2) {
+  f = mse1 / mse2
+  num.df = df1
+  den.df = df2
+} else {
+  f = mse2 / mse1
+  num.df = df2
+  den.df = df1
+}
+
+f; num.df; den.df
+
+cv = qf(1-(alpha/2), df1=num.df, df2=den.df)
+cv
+
+pvalue = (1 - pf(f, df1=num.df, df2=den.df))
+pvalue = pvalue * 2  # For two-tailed test only.
+pvalue
+
+if (pvalue < alpha) {
+  print ("Reject H0. There IS evidence of heteroscedasticity.")
+} else {
+  print ("Do not reject H0. There is insufficient evidence of heteroscedasticity. Variances appear to be equal.")
+}
+
+
+
+
+
 
 vif(complete.model)
 #Vifs are acceptable.
@@ -45,3 +133,8 @@ prediction = data.frame(
   ZSMHC=1100
 )
 predict(complete.model, prediction, interval="confidence", level=.95)
+
+
+#outliers and influential observations
+standardized.residuals = rstandard(model)
+print(standardized.residuals)
--- Transformations.r
+++ Transformations.r
@@ -14,12 +14,43 @@
 #   > Data <- cbind(Data, 1/sqrt(Data$GPA) )
 
 # IPOV
+thads <- cbind(thads, sqrt(thads$IPOV) )
 
+ggplot(data=thads, aes(x=IPOV, y=VALUE)) + geom_point() +
+  xlab('Poverty Income') +
+  ylab('Value of Unit') +
+  ggtitle('Poverty Income vs Value of Unit')
 
 # NUNITS
+thads <- cbind(thads, log(thads$NUNITS) )
 
+ggplot(data=thads, aes(x=NUNITS, y=VALUE)) + geom_point() +
+  xlab('Number Of Units in Building') +
+  ylab('Value of Unit') +
+  ggtitle('Number of Units in Structure vs Value of Unit')
 
 # ROOMS
+thads <- cbind(thads, sqrt(thads$ROOMS) )
+
+ggplot(data=thads, aes(x=ROOMS, y=PER)) + geom_point() +
+  xlab('Rooms') +
+  ylab('Persons') +
+  ggtitle('Number of Rooms Vs Number of Persons')
 
 
 # BEDRMS
+thads <- cbind(thads, sqrt(thads$BEDRMS) )
+
+ggplot(data=thads, aes(x=factor(BEDRMS), y=VALUE)) + geom_boxplot() +
+  xlab('Number Of Bed Rooms')+
+  ylab('Value of Unit') +
+  ggtitle('Number of Bedrooms (BEDRMS) vs. Value')
+
+
+#Age
+thads <- cbind(thads, 1/sqrt(thads$AGE1) )
+
+ggplot(data=thads, aes(x=AGE1, y=VALUE)) + geom_point() +
+  xlab('Age of Head Of Household')+
+  ylab('Value of Unit') +
+  ggtitle('Age of Head of Household (AGE1)) vs. Value')
